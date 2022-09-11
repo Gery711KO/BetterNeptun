958a84df1bee25bac4fe3af7bf0f9c13405fa3ca
Gery711KO <96139474+Gery711KO@users.noreply.github.com>
2022-09-12 01:04:23 +0200

    fixes for message detail

--- app/src/main/java/hu/kocsisgeri/betterneptun/ui/messages/detail_dialog/MessageDetailFragment.kt
+++ app/src/main/java/hu/kocsisgeri/betterneptun/ui/messages/detail_dialog/MessageDetailFragment.kt
@@ -1,21 +1,30 @@
 package hu.kocsisgeri.betterneptun.ui.messages.detail_dialog
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.addCallback
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import hu.kocsisgeri.betterneptun.R
 import hu.kocsisgeri.betterneptun.databinding.FragmentMessageDetailBinding
 import hu.kocsisgeri.betterneptun.ui.adapter.cell.toDateString
+import hu.kocsisgeri.betterneptun.ui.main.MainActivity
 import hu.kocsisgeri.betterneptun.utils.setBackButton
 import hu.kocsisgeri.betterneptun.utils.setTextAndAddClickableLinks
+import timber.log.Timber
 
-class MessageDetailFragment : Fragment() {
+class MessageDetailFragment : DialogFragment() {
 
     private lateinit var binding: FragmentMessageDetailBinding
     private val args by navArgs<MessageDetailFragmentArgs>()
+
+    override fun getTheme(): Int = R.style.AppDialogTheme
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,8 +36,37 @@ class MessageDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setBackButton(binding.backButton)
         setData()
+        openAnimation()
+        closeButton()
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return object : Dialog(requireContext(), theme) {
+            override fun onBackPressed() {
+                closeAnimation()
+            }
+        }
+    }
+
+    private fun openAnimation() {
+        binding.animationRoot.scaleX = 0.2f
+        binding.animationRoot.scaleY = 0.2f
+        binding.animationRoot.animate().scaleX(1f).scaleY(1f).apply {
+            duration = 300
+        }.start()
+    }
+
+    private fun closeButton() {
+        binding.closeButton.setOnClickListener {
+           closeAnimation()
+        }
+    }
+
+    private fun closeAnimation() {
+        binding.animationRoot.animate().scaleX(0.0f).scaleY(0.0f).apply {
+            duration = 300
+        }.withEndAction { findNavController().popBackStack() }.start()
     }
 
     private fun setData() {
@@ -36,7 +74,5 @@ class MessageDetailFragment : Fragment() {
         binding.subject.text = args.message?.subject
         binding.sender.text = args.message?.name
         binding.date.text = args.message?.date?.toDateString()
-
-        binding.subjectTitle.text = args.message?.subject
     }
 }
