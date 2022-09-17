b23873325e6547bb2ca9076525a07dab92515df2
Gery711KO <96139474+Gery711KO@users.noreply.github.com>
2022-09-17 04:19:08 +0200

    fixes and version bump

--- buildSrc/src/main/kotlin/hu/kocsisgeri/betterneptun/Release.kt
+++ buildSrc/src/main/kotlin/hu/kocsisgeri/betterneptun/Release.kt
@@ -3,7 +3,7 @@ import java.time.format.DateTimeFormatter
 
 object Release {
     private const val versionMajor = 0
-    private const val versionMinor = 2
+    private const val versionMinor = 3
     private const val versionPatch = 0
 
     val versionCode = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHH")).toInt()
